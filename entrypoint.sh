24bac83605a71c0bc9affaeb075fcaadf770853a
Christian Jensen <christian@noviconnect.com>
2024-06-12 15:37:58 -0700

    Environmental testing

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -o errexit
+# Ensure that ERR traps are inherited by functions, command substitutions, and subshell environments
+set -o errtrace
+# Treat unset variables as an error when substituting
+set -o nounset
+# Consider a pipeline to fail if any of the commands fail
+set -o pipefail
+# Print shell input lines as they are read
+#set -o verbose
+# Print commands and their arguments as they are executed
+#set -o xtrace
+
+echo "****** Current Directory ******"
+pwd
+
+echo "****** Listing files ******"
+ls -la
+
+echo "****** Environment Variables ******"
+env
